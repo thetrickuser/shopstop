3407a28908700142725f349b59e9a50e21488c47
thetrickuser <coolady1807@outlook.com>
2024-02-23 21:20:04 +0530

    category bar scroll added

--- src/components/CategoryBar/CategoryBar.jsx
+++ src/components/CategoryBar/CategoryBar.jsx
@@ -1,11 +1,12 @@
-import categoryData from "./CategoryData.json";
+import categoryData from "../../store/CategoryData.json";
 import ProductCategory from "./ProductCategory";
-import styles from './CategoryBar.module.scss';
+import styles from "./CategoryBar.module.scss";
 const CategoryBar = () => {
   return (
     <div className={styles.categoryBar}>
       {categoryData.map((category) => (
         <ProductCategory
+          key={category.title}
           img={category.img}
           title={category.title}
           subcategory={category.subcategory}
